2dfb5e41c40c088fb1526522f67fa3e574e81793
YashSaxena1012 <yash10decsaxena@gmail.com>
2024-03-13 18:13:08 +0530

    Button Fixed

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react'
 import { ReactNode } from 'react'
 
-type PropsType={children:ReactNode}
-const Button = ({children}:PropsType) => {
+type PropsType={
+  children:ReactNode;
+onClick?:()=>void;
+}
+const Button = ({children,onClick}:PropsType) => {
   return (
     <button style={{backgroundColor:"black",color:"white",padding:"10px 20px",cursor:"pointer"}}>{children}</button>
   )
