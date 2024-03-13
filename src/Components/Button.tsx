cf155dc6011c032e69effc0e18384c279792b771
YashSaxena1012 <yash10decsaxena@gmail.com>
2024-03-13 18:20:30 +0530

    Button fixed

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,15 +1,25 @@
-import * as React from 'react'
-import { ReactNode } from 'react'
+import * as React from "react";
+import { ReactNode } from "react";
 
-type PropsType={
-  children:ReactNode;
-onClick?:()=>void;
-}
-const Button = ({children,onClick}:PropsType) => {
+type PropsType = {
+  children: ReactNode;
+  onClick?: () => void;
+};
+
+const Button = ({ children, onClick }: PropsType) => {
   return (
-    <button style={{backgroundColor:"black",color:"white",padding:"10px 20px",cursor:"pointer"}}>{children}</button>
-  )
-}
+    <button
+    onClick={onClick}
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        padding: "10px 20px",
+        cursor: "pointer",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
 
-
-export  {Button}
+export { Button };
